7664a9d42db5e31ee5d1a2478e4d548264fa52d7
Sirecia <sireciayes@gmail.com>
2024-06-14 02:48:45 -0700

    Playing with a bunch of color options and implementing the gradient C:

--- DiceRollerHandmade/Form1.Designer.cs
+++ DiceRollerHandmade/Form1.Designer.cs
@@ -27,8 +27,10 @@
             // 
             // textBoxSides
             // 
-            textBoxSides.BackColor = SystemColors.Window;
+            textBoxSides.BackColor = SystemColors.InfoText;
+            textBoxSides.BorderStyle = BorderStyle.FixedSingle;
             textBoxSides.Font = new Font("Unispace", 14.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            textBoxSides.ForeColor = Color.Red;
             textBoxSides.Location = new Point(73, 66);
             textBoxSides.Margin = new Padding(4, 3, 4, 3);
             textBoxSides.Name = "textBoxSides";
@@ -39,30 +41,37 @@
             // 
             // buttonRoll
             // 
+            buttonRoll.BackColor = Color.Black;
+            buttonRoll.FlatStyle = FlatStyle.Flat;
             buttonRoll.Font = new Font("Unispace", 23.9999962F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            buttonRoll.ForeColor = Color.Red;
             buttonRoll.Location = new Point(73, 112);
             buttonRoll.Margin = new Padding(4, 3, 4, 3);
             buttonRoll.Name = "buttonRoll";
             buttonRoll.Size = new Size(179, 56);
             buttonRoll.TabIndex = 1;
             buttonRoll.Text = "Roll";
-            buttonRoll.UseVisualStyleBackColor = true;
+            buttonRoll.UseVisualStyleBackColor = false;
             buttonRoll.Click += buttonRoll_Click;
             // 
             // labelResult
             // 
             labelResult.AutoSize = true;
+            labelResult.BackColor = Color.Transparent;
             labelResult.Font = new Font("Unispace", 14.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
-            labelResult.Location = new Point(101, 217);
+            labelResult.ForeColor = Color.LightGray;
+            labelResult.ImageAlign = ContentAlignment.MiddleRight;
+            labelResult.Location = new Point(98, 216);
             labelResult.Margin = new Padding(5, 0, 5, 0);
             labelResult.Name = "labelResult";
-            labelResult.Size = new Size(142, 23);
+            labelResult.Size = new Size(106, 23);
             labelResult.TabIndex = 2;
-            labelResult.Text = "Result Here";
+            labelResult.Text = "Result: ";
             // 
             // label1
             // 
             label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
             label1.Font = new Font("Unispace", 15.7499981F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label1.Location = new Point(85, 25);
             label1.Name = "label1";
@@ -74,7 +83,7 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.FromArgb(255, 192, 192);
+            BackColor = Color.White;
             ClientSize = new Size(325, 267);
             Controls.Add(label1);
             Controls.Add(labelResult);
@@ -84,6 +93,7 @@
             Margin = new Padding(4, 3, 4, 3);
             Name = "MainForm";
             Text = "Dice Roller!";
+            Paint += MainForm_Paint;
             Resize += MainForm_Resize;
             ResumeLayout(false);
             PerformLayout();
--- DiceRollerHandmade/Form1.cs
+++ DiceRollerHandmade/Form1.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Drawing.Drawing2D;
 using System.Windows.Forms;
 
 namespace DiceRollerGUI
@@ -12,12 +13,33 @@ namespace DiceRollerGUI
             InitializeComponent();
             random = new Random();
             CenterLabel();
+            this.Paint += new PaintEventHandler(MainForm_Paint);
         }
+
+        private void MainForm_Paint(object sender, PaintEventArgs e)
+        {
+            // Define the gradient colors and direction
+            Color color1 = Color.FromArgb(255, 0, 64); // Red
+            Color color2 = Color.FromArgb(0, 0, 0); // Blue
+            LinearGradientBrush brush = new LinearGradientBrush(
+                this.ClientRectangle,
+                color1,
+                color2,
+                LinearGradientMode.Vertical);
+
+            // Fill the background with the gradient
+            e.Graphics.FillRectangle(brush, this.ClientRectangle);
+
+            // Clean up resources
+            brush.Dispose();
+        }
+
+
 
         private void CenterLabel()
         {
             labelResult.Left = (this.ClientSize.Width - labelResult.Width) / 2;
-            labelResult.Top = this.ClientSize.Height - labelResult.Height - 20; // Adjust 20 to change distance from bottom
+            labelResult.Top = this.ClientSize.Height - labelResult.Height - 10; // Adjust 20 to change distance from bottom
         }
 
         private void buttonRoll_Click(object sender, EventArgs e)
@@ -41,6 +63,11 @@ namespace DiceRollerGUI
         }
 
         private void textBoxSides_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainForm_Paint_1(object sender, PaintEventArgs e)
         {
 
         }
